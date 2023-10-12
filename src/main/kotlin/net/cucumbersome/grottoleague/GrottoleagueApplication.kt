42c7bc225b868de515eb1229448b1656388a3424
Michal Gutowski <michal@cucumbersome.net>
2023-10-12 19:42:41 +0200

    Initial commit

---
+++ src/main/kotlin/net/cucumbersome/grottoleague/GrottoleagueApplication.kt
@@ -0,0 +1,11 @@
+package net.cucumbersome.grottoleague
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrottoleagueApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrottoleagueApplication>(*args)
+}
---
+++ src/test/kotlin/net/cucumbersome/grottoleague/GrottoleagueApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.cucumbersome.grottoleague
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrottoleagueApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
