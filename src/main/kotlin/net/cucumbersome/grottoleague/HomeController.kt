97f189c417100a1c444fa36ddaf18bfad7b8dae2
Michal Gutowski <michal@cucumbersome.net>
2023-10-28 11:15:28 +0200

    Styling

--- src/main/kotlin/net/cucumbersome/grottoleague/HomeController.kt
+++ src/main/kotlin/net/cucumbersome/grottoleague/HomeController.kt
@@ -1,16 +1,18 @@
 package net.cucumbersome.grottoleague
 
 import net.cucumbersome.grottoleague.matches.getallmatches.GetAllMatchesService
+import net.cucumbersome.grottoleague.matches.ranking.PrepareRankingService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HomeController(val getAllMatchesService: GetAllMatchesService) {
+class HomeController(val getAllMatchesService: GetAllMatchesService, val prepareRankingService: PrepareRankingService) {
     @GetMapping("/")
     fun home(model: Model): String {
         model["matches"] = getAllMatchesService.getAllMatches()
+        model["ranking"] = prepareRankingService.getRanking()
         return "home"
     }
 }
